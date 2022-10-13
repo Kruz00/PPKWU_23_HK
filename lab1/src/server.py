a5892b9bee3bfc7802bb7dc1a481dc12f4c74030
Hubert Krauza <hubertkrauza@gmail.com>
2022-10-13 13:51:32 +0200

    add time show at server

--- lab1/src/server.py
+++ lab1/src/server.py
@@ -2,6 +2,7 @@
 import http.server
 import socketserver
 import os
+from time import strftime, time
 
 #print('source code for "http.server":', http.server.__file__)
 
@@ -16,7 +17,7 @@ class web_server(http.server.SimpleHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
             self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
+            self.wfile.write(("Hello World!\n" + strftime("%H:%M:%S")).encode("UTF-8"))
         else:
             super().do_GET()
     
