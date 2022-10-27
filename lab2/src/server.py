fbb5f8e1a3171c23f819138bb99f7b8ee864e218
Hubert Krauza <hubertkrauza@gmail.com>
2022-10-27 13:19:00 +0200

    lab2 done proper parameter handling with actions

--- lab2/src/server.py
+++ lab2/src/server.py
@@ -12,7 +12,12 @@ app = Flask(__name__)
 def hello_world():
     cmd_param = request.args.get('cmd', type=str)
     str_param = request.args.get('str', type=str)
-    return "<p>Hello World! " + str(cmd_param) + " " + str(str_param) + "\n" + strftime("%H:%M:%S") + "</p>"
+    if str(cmd_param) == "time":
+        return "<p>" + strftime("%H:%M:%S") + "</p>"
+    if str(cmd_param) == "rev":
+        return "<p>" + str(str_param)[::-1] + "</p>"
+
+    return "<p>Hello World! " + str(cmd_param) + " " + str(str_param) + "\n" + "</p>"
 
 
 # --- main ---
