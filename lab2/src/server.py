298356ebd5dc8de3f54c4edfd1b60b58ed18e362
Hubert Krauza <hubertkrauza@gmail.com>
2022-10-27 12:26:41 +0200

    lab2 init

---
+++ lab2/src/server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import os
+from time import strftime, time
+
+#print('source code for "http.server":', http.server.__file__)
+
+class web_server(http.server.SimpleHTTPRequestHandler):
+    
+    def do_GET(self):
+
+        print(self.path)
+        
+        if self.path == '/':
+            self.protocol_version = 'HTTP/1.1'
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=UTF-8")
+            self.end_headers()            
+            self.wfile.write(("Hello World!\n" + strftime("%H:%M:%S")).encode("UTF-8"))
+        else:
+            super().do_GET()
+    
+# --- main ---
+
+PORT = 4080
+
+print(f'Starting: http://localhost:{PORT}')
+
+tcp_server = socketserver.TCPServer(("",PORT), web_server)
+tcp_server.serve_forever()
